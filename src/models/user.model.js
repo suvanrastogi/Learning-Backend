80042620ddc618269946b1b129e4c0b8611ecaaf
Suvan Rastogi <rastogisuvan224@gmail.com>
2026-09-17 16:40:49 +0530

    completing user.models.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,12 +50,36 @@ const userSchema = new mongoose.Schema(
 
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();44
+    if (!this.isModified("password")) return next();
     this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname
+  },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+    expiresIn: process.env.ACCESS_TOKEN_SECRET
+  })
+ }
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_SECRET,
+      }
+    );
 }
 export const User = mongoose.model("User", userSchema);
